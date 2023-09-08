54e2337984616449b187297153f3c645d753a552
Jan Moeller <jan.moeller0@gmail.com>
2023-09-08 10:36:34 +0200

    feat: use lqip for image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,42 @@
-import { component$, useStylesScoped$ } from "@builder.io/qwik";
+import {
+  component$,
+  useStylesScoped$,
+  useSignal,
+  useVisibleTask$,
+} from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import styles from "./index.css?inline";
 
-import imgUrl from "../../public/patrizier_hex.webp?inline";
+import img from "../../public/patrizier_hex.webp?lqip";
 
 import { SiGithub, SiKeybase } from "@qwikest/icons/simpleicons";
 
 export default component$(() => {
   useStylesScoped$(styles);
+  const imgRef = useSignal<HTMLImageElement>();
+  const loaded = useSignal(false);
+  useVisibleTask$(() => {
+    imgRef.value!.decode().then(() => {
+      loaded.value = true;
+    });
+  });
+
   return (
     <div class="index">
-      <img src={imgUrl}></img>
+      <img
+        ref={imgRef}
+        src={img.src}
+        style={
+          loaded.value
+            ? {}
+            : {
+                background: `url("${img.lqip}")`,
+                backgroundSize: "7.5rem",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }
+        }
+      />
       <div class="post-line"></div>
       <div class="links">
         <a href="https://github.com/Croissong">
