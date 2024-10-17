8b9fcbc5aa139b98965b7914523202e4692bb417
Jan Moeller <jan.moeller0@gmail.com>
2024-10-17 17:46:32 +0200

    chore: bump deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,6 +27,7 @@ module.exports = {
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
     "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-this-alias": "off",
