dc86f0946a645fe8d1f3930be0638381344ed2d1
Jan Moeller <jan.moeller0@gmail.com>
2025-08-21 21:50:50 +0200

    feat: add projects section

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,17 +2,17 @@ import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import styles from "./index.css?inline";
 
-import Image from "/src/media/images/patrizier_hex.png?format=avif&w=256&inline&quality=50&jsx"
-
+import Image from "/src/media/images/patrizier_hex.png?format=avif&w=256&inline&quality=50&jsx";
 
 import { SiGithub, SiKeybase } from "@qwikest/icons/simpleicons";
+import Projects from "./projects";
 
 export default component$(() => {
   useStylesScoped$(styles);
   return (
     <main class="index">
       <a href="/images/patrizier.png" title="Patrician logo HQ">
-        <Image loading="eager" alt="Patrician logo"/>
+        <Image loading="eager" alt="Patrician logo" />
       </a>
       <div class="links">
         <a href="https://github.com/Croissong">
@@ -22,6 +22,8 @@ export default component$(() => {
           <SiKeybase />
         </a>
       </div>
+
+      <Projects />
     </main>
   );
 });
---
+++ src/routes/projects.tsx
@@ -0,0 +1,30 @@
+import { component$, useStyles$ } from "@builder.io/qwik";
+
+import styles from "./projects.css?inline";
+
+export default component$(() => {
+  useStyles$(styles);
+
+  return (
+    <section class="projects">
+      <h2>Projects</h2>
+      <ul class="project-links">
+        <li>
+          <a href="https://github.com/croissong/gitwatch-rs">gitwatch-rs</a> -
+          CLI to watch a local git repo and automatically commit changes
+        </li>
+        <li class="personal-projects">
+          <a href="https://github.com/croissong/dotfiles">dotfiles</a> - There's
+          no place like $HOME
+        </li>
+        <li>
+          <a href="https://github.com/croissong/system">nixos-config</a> - I use
+          NixOS btw
+        </li>
+        <li>
+          <a href="https://github.com/croissong/emacs">emacs</a> &gt; Vim
+        </li>
+      </ul>
+    </section>
+  );
+});
